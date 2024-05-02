968e2c226d4806151b7aaa06cf7accb305e68aa4
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-02 10:41:44 +0200

    Filter for prime numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {upper_bound}");
+    let mut primes = vec![true; (upper_bound + 1) as usize];
+    let mut result = Vec::new();
+    
+    for p in 2..=upper_bound {
+        if primes[p as usize] {
+            result.push(p);
+            let mut multiple = p * p;
+            while multiple <= upper_bound {
+                primes[multiple as usize] = false;
+                multiple += p;
+            }
+        }
+    }
+    
+    result
 }
